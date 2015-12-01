3e76dd78cb20c1b16298095d25a0705eeeb99957
onyoo <roberto9m9@gmail.com>
2015-12-01 23:50:24 +0100

    really working?

--- lib/stock-scraper/cli.rb
+++ lib/stock-scraper/cli.rb
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 class CLI
   def initialize
-    puts "I'm working"
+    puts "I'm REALLY working"
   end
 end
-
-CLI.new
-=======
-class CLI 
-  def initialize
-    puts "khjasdlkhasd"
-  end
-end
->>>>>>> 3f86f3f7be6c9f337240e7b33e8a25d0e9a00db7
