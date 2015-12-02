2d0c24e592d653854a92d579c638c81c57f70035
onyoo <roberto9m9@gmail.com>
2015-12-02 13:47:50 +0100

    translates extra tags to english

--- lib/stock-scraper/cli.rb
+++ lib/stock-scraper/cli.rb
@@ -1,17 +1,13 @@
 class CLI
-<<<<<<< HEAD
-  attr_accessor :tickers, :data_type
-
-  def initialize
-    @data_type = ["n","a","b"]
-=======
   attr_accessor :tickers, :tags
 
   STOCKS = ["AAPL", "GOOG", "MSFT", "NFLX", "TSLA", "IBM", "AMZN", "EBAY", "SBUX", "FB"]
+  TAGS_ENGLISH = []
+  TAG_VALUES = []
 
   def initialize 
-    @tags = %w(napot8mwvj1)
->>>>>>> fffab732bb723502579721772d9f73e3e3f70757
+    @tags = ["n","a","p","o","t8","m","w","v","j1"]
+    @origional_length = @tags.length
   end
 
   def call
@@ -19,46 +15,31 @@ class CLI
   end
 
   def list
-<<<<<<< HEAD
-#### eventually lists the day's hottest stocks####
-    puts ""
-    puts "Here are some of the bigger stocks:"
-    puts "aapl, goog, nke, tsla"
-    puts "Please enter the tickers you want info on. Seperate with commas ','"
-    @tickers = gets.strip.strip.upcase.split(",").join("+")
-
-    puts "Thanks. We'll give you the basic info, but please select any other info you may want?"
-    puts "Seperate with commas ','"
-    puts "e: Earnings per Share"
-    puts "y: Dividend Yield"
-    puts "d: Dividend per Share"
-    puts "m6: Percent Change From 200 Day Moving Average"
-    puts "r2: P/E Ratio (Realtime)"
-    z = gets.strip.split(",")
-    (@data_type << z).flatten!
-=======
     #### eventually lists the day's hottest stocks ####
     puts "\n"
-    puts " Most Popular Stocks".center(37)
-    puts " " + "-" * 35
+    puts " Most Popular Stocks  |  Trending Stocks ".center(47)
+    puts " " + "-" * 45
     puts "\n"
     STOCKS.each_with_index{ |stock, i| puts " #{i+1}. #{stock}" }
->>>>>>> fffab732bb723502579721772d9f73e3e3f70757
   end
 
   def start
     list
-<<<<<<< HEAD
-    url = "http://finance.yahoo.com/d/quotes.csv?s=#{tickers}&f=#{data_type.join("")}"
-=======
     get_tickers
     get_tags
+    tag_translate
     print_stocks
+  end
+
+keys = TAGS_ENGLISH
+values = TAG_VALUES
+
+  def create_hash
+
   end
 
   def print_stocks
     url = "http://finance.yahoo.com/d/quotes.csv?s=#{tickers}&f=#{tags.join}"
->>>>>>> fffab732bb723502579721772d9f73e3e3f70757
     info = open(url)
 
     open(url) do |f| 
@@ -67,6 +48,8 @@ class CLI
           puts "\n" 
           puts "#{row[0]}".center(61)
           puts " " + "-" * 65
+
+
           puts " Asking price:     $#{row[1]}       Days Range:      $#{row[5]}" 
           puts " Previous close:   $#{row[2]}       52 Week Range:   $#{row[6]}"
           puts " Open:             $#{row[3]}       Volume:          #{row[7]}"
@@ -74,10 +57,27 @@ class CLI
           puts 
         end
       end
+      binding.pry
+      z=4
     end
   end
-<<<<<<< HEAD
-=======
+
+  def tag_translate
+    tags[@origional_length..-1].each do |tag|
+      case tag
+      when "h0"
+        TAGS_ENGLISH << "Earnings per Share"
+      when "d"
+        TAGS_ENGLISH << "Dividend per Share"
+      when "y"
+        TAGS_ENGLISH << "Dividend Yield"
+      when "r2"
+        TAGS_ENGLISH << "P/E Ratio (Realtime)"
+      when "m6"
+        TAGS_ENGLISH << "Percent Change From 200 Day Moving Average"
+      end
+    end
+  end
 
   def get_tickers 
     puts "\nEnter the stock tickers you want more infomation on." 
@@ -96,7 +96,6 @@ class CLI
     puts " r2 | P/E Ratio (Realtime)"
     puts " m6 | Percent Change From 200 Day Moving Average\n\n"
     
-    tags << gets.strip
+    (tags << gets.strip.split(",")).flatten!
   end
->>>>>>> fffab732bb723502579721772d9f73e3e3f70757
 end
