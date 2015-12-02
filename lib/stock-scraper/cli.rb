fdcb19c2b5ff6fbe8d402fd762e0194d74c11edf
frankNowinski <nowinski.frank@gmail.com>
2015-12-01 20:17:06 -0500

    updated list and start

--- lib/stock-scraper/cli.rb
+++ lib/stock-scraper/cli.rb
@@ -1,28 +1,46 @@
 class CLI
+
+  STOCKS = [
+    "AAPL",
+    "GOOG",
+    "MSFT",
+    "NFLX",
+    "TSLA",
+    "IBM",
+    "AMZN",
+    "EBAY",
+    "SBUX",
+    "FB"
+  ]
   def call
     start
   end
 
   def list
 #### eventually lists the day's hottest stocks####
-    puts ""
-    puts "here are some of the bigger stocks:"
-    puts "aapl, goog, nke, tsla"
-    puts "please enter the tickers you want info on."
+    puts "\n"
+    puts " Most Popular Stocks".center(37)
+    puts " " + "-" * 35
+    puts "\n"
+    STOCKS.each_with_index{ |stock, i| puts " #{i+1}. #{stock}" }
   end
 
   def start
     list
-    answer = gets.strip.upcase
-    url = "http://finance.yahoo.com/d/quotes.csv?s=#{answer}&f=naby"
+
+    begin 
+      puts "\n\nEnter the stock ticker you want more info on:"
+      answer = gets.strip.upcase
+    end until answer.match(/[a-zA-Z]/) && answer.length <= 4
+
+    url = "http://finance.yahoo.com/d/quotes.csv?s=#{answer}&f=nab"
     info = open(url)
-    open(url) do |f|
-      f.each_line do |l|
-        CSV.parse(l) do |row|
+    open(url) do |f| 
+      f.each_line do |l| 
+        CSV.parse(l) do |row| 
           puts row
         end
       end
     end
   end
-
 end
